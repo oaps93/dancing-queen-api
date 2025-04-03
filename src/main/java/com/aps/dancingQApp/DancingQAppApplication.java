df6725289da16652c6c067aa62c34c99c365b5f5
oaps93_surfacehome <oaps93@gmail.com>
2025-04-03 16:15:43 -0600

    first commit

---
+++ src/main/java/com/aps/dancingQApp/DancingQAppApplication.java
@@ -0,0 +1,13 @@
+package com.aps.dancingQApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DancingQAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DancingQAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aps/dancingQApp/DancingQAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aps.dancingQApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DancingQAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
